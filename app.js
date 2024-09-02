1238e48dcf40f7db29d7fe931e7420db3ab4c8c6
Sanjeev Shrestha <sanjeev1.785401@cct.tu.edu.np>
2024-09-02 22:44:20 +0545

    home page

--- app.js
+++ app.js
@@ -11,20 +11,28 @@ require("./model/index.js")
 app.use(express.urlencoded({ extended: true })) //when both frontend and backend is in monolitic format else express.json
 app.set("view engine", "ejs")
 
+app.get("/", async (req, res) => {
+  const datas = await blogs.findAll()
+  res.render("home.ejs", { blogs: datas })
+})
+
 app.get("/create", (req, res) => {
   res.render("create.ejs")
 })
 
 app.post("/create", upload.single("image"), async (req, res) => {
+  const filename = req.file.filename
   const { title, subtitle, description } = req.body
   await blogs.create({
     title: title,
     subtitle: subtitle,
     description: description,
+    image: filename,
   })
   res.send("Blog added successfully")
 })
 app.use(express.static("public/css/"))
+app.use(express.static("./storage/"))
 
 app.listen(3000, () => {
   console.log("listening in port hoii tw")
